9a5e6180e04ab53433ea3d22ae344878585ab334
Agu <aguchukwuemekag@gmail.com>
2023-01-02 21:57:11 +0100

    new changes

--- components/ThemePicker.tsx
+++ components/ThemePicker.tsx
@@ -5,14 +5,16 @@ import {AiOutlineCheckCircle} from "react-icons/ai"
 
 const ThemePicker = ({onChange = () => {}}: any ) => {
     const [selected, setSelected] = React.useState<string>("")
-    const colors = [
-        "#310099",
-        "#FFDF39",
-        "#D0B8AC",
-        "#990000",
-        "#282828",
-        "#004F24",
-    ]
+    // const colors = [
+    //     "#310099",
+    //     "#FFDF39",
+    //     "#D0B8AC",
+    //     "#990000",
+    //     "#282828",
+    //     "#004F24",
+    // ]
+
+    const colors = ['#FF6900', '#FCB900', '#7BDCB5', '#00D084', '#8ED1FC', '#0693E3', '#ABB8C3', '#EB144C', '#F78DA7', '#9900EF']
 
     React.useEffect(() => {
 
@@ -21,9 +23,9 @@ const ThemePicker = ({onChange = () => {}}: any ) => {
     }, [selected])
     
   return (
-    <div className='flex items-center gap-3 mt-3'>
+    <div className='flex items-center gap-3 mt-3 flex-wrap'>
         {colors.map((color, index) => (
-            <div onClick={() => setSelected(color)} key={index} className={`h-8 w-8 ${selected === color && ("border-white border-2")} flex items-center justify-center rounded ${'bg-['+color+']'}`}>
+            <div style={{backgroundColor:color}} onClick={() => setSelected(color)} key={index} className={`h-8 w-8 ${selected === color && ("border-white border-2")} flex  items-center justify-center rounded`}>
                 {selected === color && (<AiOutlineCheckCircle  className='text-white' />)}
             </div>
         ))}
--- pages/cards/new.tsx
+++ pages/cards/new.tsx
@@ -1,29 +1,87 @@
-import React from 'react'
-import ThemePicker from '../../components/ThemePicker'
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import ThemePicker from "../../components/ThemePicker";
+import {BsWallet2} from "react-icons/bs"
 
 function newCard() {
+  const router = useRouter();
+
+  const [step, setStep] = useState<number>(0);
+
+  const nextStep = () => {
+    setStep((prev) => prev + 1);
+  };
+
   return (
-    <div className='h-screen bg-gray-900 text-white px-8 py-8 flex items-center justify-center'>
-        <div className='max-w-2xl'>
-            
-            <h3 className='font-semibold md:text-xl text-lg text-gray-200 mt-1'>Create a new card</h3>
+    <div className="h-screen bg-gray-900 relative text-white px-8 py-8 flex items-center justify-center">
+      <button
+        onClick={() => router.back()}
+        className="absolute top-4 right-4 font-semibold"
+      >
+        Cancel
+      </button>
+      <div className="max-w-2xl">
+        <div className="mt-6 w-full px-3">
+          {step === 0 && (
+            <>
+              <h3 className="font-semibold md:text-xl mb-4 text-lg text-gray-200 mt-1">
+                Create a new card
+              </h3>
+              <div className="mx-auto md:w-[400px] sm:w-[320px] w-full">
+                <label className="font-semibold text-gray-200">
+                  Pick a theme
+                </label>
+                <ThemePicker onChange={(val: any) => console.log(val)} />
+              </div>
+              <div className="mx-auto mt-5 md:w-[400px] sm:w-[320px] w-full">
+                <label className="font-semibold text-gray-200">
+                  Name on card
+                </label>
+                <input
+                  maxLength={50}
+                  placeholder="eg. John Doe"
+                  className="h-12 w-full mx-auto border bg-transparent mt-2 rounded px-4"
+                />
+              </div>
+              <button
+                onClick={() => nextStep()}
+                className="bg-blue-300 font-medium text-slate-800 mt-5 ml-auto block px-4 py-2 rounded-full"
+              >
+                Next
+              </button>
+            </>
+          )}
 
-            <div className='mt-6 w-full px-3'>
-
-                 <div className="mx-auto md:w-[400px] sm:w-[320px] w-full">
-                 <label className='font-semibold text-gray-200'>Pick a theme</label>
-                 <ThemePicker onChange={(val:any) => console.log(val)
-                 } />
-                </div>
-                <div className="mx-auto mt-5 md:w-[400px] sm:w-[320px] w-full">
-                    <label className='font-semibold text-gray-200'>Name on card</label>
-                    <input placeholder='eg. John Doe' className='h-12 w-full mx-auto border bg-transparent mt-2 rounded px-4' />
-                </div>
-            </div>
-
+          {step === 1 && (
+            <>
+              <h3 className="font-semibold md:text-xl mb-4 text-lg text-gray-200 mt-1">
+                Fund your card
+              </h3>
+              <h5 className="text-gray-300 text-sm">Select wallet to fund your card</h5>
+              <div className="mx-auto mt-6 md:w-[400px] sm:w-[320px] w-full">
+                <button className="h-14 flex items-center gap-4  px-4 rounded bg-slate-700 border border-gray-300 w-full">
+                    <img src="/logo-light.svg" className="h-8 w-auto" />
+                     <div>
+                        <h3 className="text-left font-semibold">Gamma Wallet</h3>
+                        <h5 className="text-left text-gray-300 text-[12px]">Fund with gammapay wallet</h5>
+                     </div>
+                </button>
+                <button className="h-14 flex mt-6 items-center gap-4  px-4 rounded bg-slate-700 border border-gray-300 w-full">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Binance_Logo.svg/800px-Binance_Logo.svg.png" className="h-8 w-auto" />
+                     <div>
+                        <h3 className="text-left font-semibold">Binance Pay</h3>
+                        <h5 className="text-left text-gray-300 text-[12px]">Fund with binance wallet</h5>
+                     </div>
+                </button>
+              </div>
+              
+              
+            </>
+          )}
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default newCard
+export default newCard;
